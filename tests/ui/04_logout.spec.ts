13a6c54b0074f4954653c63248a66bb740b722ae
fcondette <fcondette>
2026-08-17 18:43:33 +0200

    fixed second bug for 04_logout.spec.ts failing in CI - user icon data-testid

--- tests/ui/04_logout.spec.ts
+++ tests/ui/04_logout.spec.ts
@@ -8,7 +8,7 @@ test("user can logout", async ({ accountMenu, page }) => {
 	await accountMenu.logout();
 	await expect(page).toHaveURL(`${process.env.BASE_URL}/`);
 
-	//checking that a click on the open Menu button redirects to /auth as no user is logged in
-	await accountMenu.openMenu();
+	//checking that a click on the user icon redirects to /auth as no user is logged in
+	await page.getByTestId("auth-link").click();
 	await expect(page).toHaveURL(`${process.env.BASE_URL}/auth`);
 });
